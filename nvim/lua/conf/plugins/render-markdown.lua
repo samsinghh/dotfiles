63843d7ad49780da93912624bb6ad1f37b8269ef
samsinghh <samyak1113@gmail.com>
2026-07-15 00:23:23 -0500

    little more sweeping

--- nvim/lua/conf/plugins/render-markdown.lua
+++ nvim/lua/conf/plugins/render-markdown.lua
@@ -7,8 +7,19 @@ return {
   ft = { "markdown" },
 
   config = function()
+       local function code_hl()
+      for _, group in ipairs({
+        "RenderMarkdownCode",
+        "RenderMarkdownCodeInline",
+      }) do
+        vim.api.nvim_set_hl(0, group, { link = "NormalFloat" })
+      end
+    end
+    code_hl()
+    vim.api.nvim_create_autocmd("ColorScheme", { callback = code_hl })
+
     require("render-markdown").setup({
-      -- raw markdown on the cursor line, rendered everywhere else
+      -- modes that render at all; insert mode shows the raw buffer
       render_modes = { "n", "c", "t" },
 
       heading = {
